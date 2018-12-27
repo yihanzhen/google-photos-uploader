8c815c13c1360b3a5dd5f3703cf5f537a92f83b3
Hanzhen Yi <hzyi@uw.edu>
2018-12-26 17:18:53 -0800

    change options to use builders

--- src/main/java/com/hzyi/google/photos/uploader/UploaderMain.java
+++ src/main/java/com/hzyi/google/photos/uploader/UploaderMain.java
@@ -21,24 +21,72 @@ public class UploaderMain {
    * </ul>
    */
   static final Option OPTION_UPLOAD_ALL =
-      new Option("a", "all", false, "upload all photos in currect directory with default album names");
+      Option.builder("a")
+          .longOpt("all")
+          .hasArg(false)
+          .required(false)
+          .desc("upload all photos in currect directory with default album names")
+          .build();
   static final Option OPTION_UPLOAD_ALBUM =
-      new Option("ab", "album", false, "upload all photos to an album. If the album does not exist, create one");
+      Option.builder("ab")
+          .longOpt("album")
+          .hasArg()
+          .required(false)
+          .desc("upload all photos to an album")
+          .build();
   static final Option OPTION_UPLOAD_APPEND =
-      new Option("ap", "append", false, "allows uploading photos to an existing album");
+      Option.builder("ap")
+          .longOpt("append")
+          .hasArg(false)
+          .required(false)
+          .desc("allow uploading to an existing album")
+          .build();
   static final Option OPTION_UPLOAD_FILE =
-      new Option("f", "file", false, "upload a certain photo. This can be an absolute path or a relative path if -d exists");
+      Option.builder("f")
+          .longOpt("file")
+          .hasArg()
+          .required(false)
+          .desc("upload a certain photo. This can be an absolute path or a relative path if -d exists")
+          .build();
   static final Option OPTION_UPLOAD_DIRECTORY =
-      new Option("dir", "directory", false, "upload all photos in a directory");
+      Option.builder("dir")
+          .longOpt("directory")
+          .hasArg()
+          .required(false)
+          .desc("change working directory to that given by this flag")
+          .build();
+
   static final Option OPTION_UPLOAD_DEBUG =
-      new Option("d", "debug", false, "print operations instead of doing the real uploads");
+      Option.builder("d")
+          .longOpt("debug")
+          .hasArg(false)
+          .required(false)
+          .desc("print operations instead of doing the real uploads")
+          .build();
+
   static final Option OPTION_UPLOAD_LOCATION =
-      new Option("l", "location", false, "prefix the location before album names");
+      Option.builder("l")
+        .longOpt("location")
+        .hasArg()
+        .required(false)
+        .desc("prefix the location before the album name")
+        .build();
+
   static final Option OPTION_UPLOAD_TIME =
-      new Option("t", "time", false, "prefix the time to before the album names");
+      Option.builder("t")
+        .longOpt("time")
+        .hasArg()
+        .required(false)
+        .desc("prefix the time before the album name")
+        .build();
 
   static final Option OPTION_LIST_PHOTO_ALBUM =
-      new Option("ab", "album", true, "listing all photos in this album");
+      Option.builder("ab")
+        .longOpt("album")
+        .hasArg()
+        .required(true)
+        .desc("list all photos in this album")
+        .build();
 
   public static void main(String[] args) throws Exception {
     if (args.length < 1) {
