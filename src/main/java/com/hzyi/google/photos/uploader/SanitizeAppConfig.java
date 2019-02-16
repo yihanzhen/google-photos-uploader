6030a7a6a32e111ab270adb8e961b03534e73695
Hanzhen Yi <hzyi@uw.edu>
2019-02-15 23:31:15 -0800

    first test case pass

--- src/main/java/com/hzyi/google/photos/uploader/SanitizeAppConfig.java
+++ src/main/java/com/hzyi/google/photos/uploader/SanitizeAppConfig.java
@@ -2,7 +2,6 @@ package com.hzyi.google.photos.uploader;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
-import java.util.List;
 import org.apache.commons.cli.CommandLine;
 
 @AutoValue
@@ -18,19 +17,42 @@ abstract class SanitizeAppConfig implements AppConfig {
 
   public abstract boolean force();
 
-  public abstract List<String> directories();
+  public abstract ImmutableList<String> directories();
 
-  public abstract List<String> processedDirectories();
+  public abstract ImmutableList<String> processedDirectories();
 
   public abstract String destinationDirectory();
 
   public static SanitizeAppConfig fromCommandLine(CommandLine cl) {
-    return new AutoValue_SanitizeAppConfig(
-        cl.hasOption(SanitizeOptions.DEBUG.getOpt()),
-        cl.hasOption(SanitizeOptions.RECURSIVE.getOpt()),
-        cl.hasOption(SanitizeOptions.FORCE.getOpt()),
-        ImmutableList.copyOf(cl.getOptionValues(SanitizeOptions.DIRECTORY.getOpt())),
-        ImmutableList.copyOf(cl.getOptionValues(SanitizeOptions.PROCESSED_DIRECTORY.getOpt())),
-        cl.getOptionValue(SanitizeOptions.DESTINATION_DIRECTORY.getOpt()));
+    return new AutoValue_SanitizeAppConfig.Builder()
+        .debug(cl.hasOption(SanitizeOptions.DEBUG.getOpt()))
+        .recursive(cl.hasOption(SanitizeOptions.RECURSIVE.getOpt()))
+        .force(cl.hasOption(SanitizeOptions.FORCE.getOpt()))
+        .directories(ImmutableList.copyOf(cl.getOptionValues(SanitizeOptions.DIRECTORY.getOpt())))
+        .processedDirectories(
+            ImmutableList.copyOf(cl.getOptionValues(SanitizeOptions.PROCESSED_DIRECTORY.getOpt())))
+        .destinationDirectory(cl.getOptionValue(SanitizeOptions.DESTINATION_DIRECTORY.getOpt()))
+        .build();
+  }
+
+  public static SanitizeAppConfig.Builder newBuilder() {
+    return new AutoValue_SanitizeAppConfig.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder debug(boolean val);
+
+    public abstract Builder recursive(boolean val);
+
+    public abstract Builder force(boolean val);
+
+    public abstract Builder directories(ImmutableList<String> val);
+
+    public abstract Builder processedDirectories(ImmutableList<String> val);
+
+    public abstract Builder destinationDirectory(String val);
+
+    public abstract SanitizeAppConfig build();
   }
 }
--- src/test/java/com/hzyi/google/photos/uploader/SanitizeAppTest.java
+++ src/test/java/com/hzyi/google/photos/uploader/SanitizeAppTest.java
@@ -1,25 +1,29 @@
 package com.hzyi.google.photos.uploader;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableSet;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class SanitizeAppTest {
+  private static final String rootStr = "/tmp/google-photos-uploader/sanitize";
+  private static final String fromDirStr = "/tmp/google-photos-uploader/sanitize/from/";
+  private static final String fromDirStr1 = "/tmp/google-photos-uploader/sanitize/from1/";
+  private static final String fromDirStr2 = "/tmp/google-photos-uploader/sanitize/from2/";
+  private static final String processedDirStr = "/tmp/google-photos-uploader/sanitize/processed/";
+  private static final String processedDirStr1 = "/tmp/google-photos-uploader/sanitize/processed1/";
+  private static final String processedDirStr2 = "/tmp/google-photos-uploader/sanitize/processed2/";
+  private static final String destDirStr = "/tmp/google-photos-uploader/sanitize/dest";
 
-  private static final String fromDirStr = "/tmp/from/";
-  private static final String fromDirStr1 = "/tmp/from1/";
-  private static final String fromDirStr2 = "/tmp/from2/";
-  private static final String processedDirStr = "/tmp/processed/";
-  private static final String processedDirStr1 = "/tmp/processed1/";
-  private static final String processedDirStr2 = "/tmp/processed2/";
-  private static final String destDirStr = "/tmp/dest";
-  
+  private static final Path root = Paths.get(rootStr);
   private static final Path fromDir = Paths.get(fromDirStr);
   private static final Path fromDir1 = Paths.get(fromDirStr1);
   private static final Path fromDir2 = Paths.get(fromDirStr2);
@@ -28,17 +32,56 @@ public class SanitizeAppTest {
   private static final Path processedDir2 = Paths.get(processedDirStr2);
   private static final Path destDir = Paths.get(destDirStr);
 
+  private static void deleteWrapper(Path path) {
+    try {
+      Files.delete(path);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to delete file due to: " + e);
+    }
+  }
+
   @Before
-  public static setUp() {
-    fromDirectory();
-    fromDirectories();
-    processedDirectory();
-    processedDirectories();
-    destDirectory();
-  } 
+  public void setUp() throws Exception {
+    if (Files.exists(root)) {
+      Files.walk(root).sorted(Comparator.reverseOrder()).forEach(SanitizeAppTest::deleteWrapper);
+    } else {
+      Files.createDirectories(root);
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (Files.exists(root)) {
+      Files.walk(root).sorted(Comparator.reverseOrder()).forEach(SanitizeAppTest::deleteWrapper);
+    }
+  }
 
   @Test
-  public void testMoveJpegs() {}
+  public void testMoveJpegs() throws Exception {
+    Files.createDirectories(fromDir);
+    Files.createDirectories(fromDir1);
+    Files.createDirectories(destDir);
+    Files.createDirectories(processedDir);
+    Files.createFile(fromDir.resolve("1111.jpg"));
+    Files.createFile(fromDir.resolve("2222.jpeg"));
+    Files.createFile(fromDir1.resolve("3333.JPG"));
+    Files.createFile(fromDir1.resolve("4444.JPEG"));
+    SanitizeAppConfig config =
+        SanitizeAppConfig.newBuilder()
+            .debug(false)
+            .force(false)
+            .recursive(false)
+            .directories(ImmutableList.of(fromDirStr, fromDirStr1))
+            .processedDirectories(ImmutableList.of(processedDirStr))
+            .destinationDirectory(destDirStr)
+            .build();
+    new SanitizeApp().run(config);
+    assertThat(
+            Files.list(destDir.resolve("original/jpeg"))
+                .map(p -> p.getFileName().toString())
+                .collect(Collectors.toList()))
+        .containsExactly("1111.jpg", "2222.jpeg", "3333.JPG", "4444.JPEG");
+  }
 
   @Test
   public void testMoveJpegsAndRaws() {}
